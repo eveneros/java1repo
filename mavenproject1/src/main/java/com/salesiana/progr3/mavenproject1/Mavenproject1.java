a08d4aea69f9d1f0149ad63c5d041536e836b4ac
Edson Veneros <veneros.edson@gmail.com>
2025-03-16 18:44:33 -0400

    Addunt new classes

---
+++ mavenproject1/src/main/java/com/salesiana/progr3/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.salesiana.progr3.mavenproject1;
+
+/**
+ *
+ * @author eveneros
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World Eds!");
+    }
+}
